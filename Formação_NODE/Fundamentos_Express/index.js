216369ba3d5ad3107f39f6989312c38ac33f64e9
eliseugomes <eliseu.uepb@gmail.com>
2021-06-29 13:46:40 -0300

    finalizando fundaentos do express e MYSQL

--- Formação_NODE/Fundamentos_Express/index.js
+++ Formação_NODE/Fundamentos_Express/index.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
 
+
+app.get("/", (req,res)=>{
+    res.send("<h1>Rumo ao primeiro job backend :D</h1>")
+})
+
+app.get("/hello/:nome?", (req, res) => {
+    const nome = req.params.nome
+    if(nome){
+        res.send(`<h1>Olá ${nome}, pronto pra codar?</h1>`)
+    }else{
+        res.send(`<h1>Olá amigo, pronto pra codar?</h1>`)
+    }
+})
+
+
 app.listen(4000, function (erro) {
     if(erro){
         console.log("ocorreu um erro")
